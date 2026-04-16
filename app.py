7c3f811530bb41837a0b9609b89f1c624e9731ce
HarshShinde2697 <harshshinde232@gmail.com>
2026-04-17 03:28:49 +0530

    changes done

--- app.py
+++ app.py
@@ -84,6 +84,6 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
     except Exception as e:
             raise NetworkSecurityException(e,sys)
 
-    
+ #changes done   
 if __name__=="__main__":
     app_run(app,host="0.0.0.0",port=8080)
